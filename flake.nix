23835c66930df4d4034732b4c16279d89259eeb0
Ingo Ruhnke <grumbel@gmail.com>
2024-01-27 17:46:19 +0100

    Update to nixos-23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Terminal-like text output for SDL";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
@@ -25,7 +25,7 @@
 
              nativeBuildInputs = with pkgs; [
                cmake
-               pkgconfig
+               pkg-config
              ];
 
              buildInputs = with pkgs; [
